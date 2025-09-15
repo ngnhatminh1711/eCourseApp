dcfde637477abcf92623c2a8f15bd85139d79edb
ngnhatminh1711 <ngnhatminh1711@gmail.com>
2025-09-16 02:12:47 +0700

    Init project and create admin view

---
+++ courseapis/courses/admin.py
@@ -0,0 +1,36 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from courses.models import Category, Course, Lesson, Tag
+
+
+class LessonForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
+class MyLessonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'create_date', 'active', 'course']
+    search_fields = ['subject']
+    list_filter = ['id', 'create_date']
+    readonly_fields = ['image_view']
+    form = LessonForm
+
+    def image_view(self, lesson):
+        return mark_safe(f"<img src='/static/{lesson.image.name}' width='200' />")
+
+    class Media:
+        css = {
+            'all': ('/static/css/style.css',)
+        }
+
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Lesson, MyLessonAdmin)
+admin.site.register(Tag)
---
+++ courseapis/courses/models.py
@@ -0,0 +1,51 @@
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(BaseModel):
+    subject = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='courses/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.subject
+
+
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=255)
+    content = RichTextField()
+    image = models.ImageField(upload_to='lessons/%Y/%m/')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.subject
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
